c7d1a28aaaedfcdc0625462d8ac4b952a1616e4a
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2020-06-12 18:02:15 +0900

    update components
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- README.md
+++ README.md
@@ -27,10 +27,13 @@ Run a pod, an Alpine container, and execute a shell within it (using crun):
 $ ./run.sh
 ...
 + ./crictl.sh exec -it d214040f8023a6938ffac05ff6db2aa0f884e86778d5f40a9a3171d2d1e8fc7c sh
-/ # 
+/ # cat /sys/fs/cgroup/cpu.weight
+5
+/ # cat /sys/fs/cgroup/memory.max
+67108864
 ```
 
-The shell is executed in a cgroup like `/user.slice/user-1001.slice/user@1001.service/user.slice/cri-containerd-d214040f8023a6938ffac05ff6db2aa0f884e86778d5f40a9a3171d2d1e8fc7c.scope`.
+The shell is executed in a cgroup like `/user.slice/user-1001.slice/user@1001.service/user.slice/cri-containerd-d214040f8023a6938ffac05ff6db2aa0f884e86778d5f40a9a3171d2d1e8fc7c.scope`. Note that `cat /proc/1/cgroup` in the container shows `/` as the cgroup because cgroup namespaces are enabled for non-privileged pods on cgroup v2.
 
 To use runc (`runc` binary need to be installed under `./bin`):
 ```console
--- bin-download.sh
+++ bin-download.sh
@@ -9,11 +9,11 @@ curl -sSL https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.18.0
 curl -sSL https://github.com/rootless-containers/rootlesskit/releases/download/v0.9.5/rootlesskit-x86_64.tar.gz | tar xzv -C bin
 
 : Downloading slirp4netns
-curl -o bin/slirp4netns -sSL https://github.com/rootless-containers/slirp4netns/releases/download/v1.1.0-beta.2/slirp4netns-x86_64
+curl -o bin/slirp4netns -sSL https://github.com/rootless-containers/slirp4netns/releases/download/v1.1.1/slirp4netns-x86_64
 chmod +x bin/slirp4netns
 
 : Downloading containerd
-curl -sSL https://github.com/kind-ci/containerd-nightlies/releases/download/containerd-1.4.0-beta.1-8-g62dd1411/containerd-1.4.0-beta.1-8-g62dd1411.linux-amd64.tar.gz | tar xzv -C bin --strip-components=1 bin/containerd bin/containerd-shim-runc-v2 bin/ctr
+curl -sSL https://github.com/kind-ci/containerd-nightlies/releases/download/containerd-1.4.0-beta.1-26-g834665d9/containerd-1.4.0-beta.1-26-g834665d9.linux-amd64.tar.gz | tar xzv -C bin --strip-components=1 bin/containerd bin/containerd-shim-runc-v2 bin/ctr
 
 : NOTE: CRI-O is not supported yet
 
